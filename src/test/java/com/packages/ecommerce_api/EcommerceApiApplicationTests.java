f771a430dcaba807b0c2e8ca06d3a5f803378fa0
Phillipe Mendonça <phillipe.mendonca20@gmail.com>
2025-02-18 13:12:20 -0300

    first commit

---
+++ src/test/java/com/packages/ecommerce_api/EcommerceApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.packages.ecommerce_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EcommerceApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
